30742b993a4fb1f3e5bd68457d56bc61f3d896d7
Akanle Jeremy <akanlejeremy@gmail.com>
2024-10-30 13:39:15 +0300

    passing

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [ cart, setCart ] = useState("")
+  
   return (
-    <li className="">
+    <li className={cart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cart ? "remove" : "add"} onClick={() => cart ? setCart("") : setCart("in-cart")}>{cart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
